ac0f10c8ecacbd0808a758e44b190ea84acd5561
Sarah Edkins <edkins.sarah@gmail.com>
2017-07-13 13:35:58 -0400

    add prod server

--- server.js
+++ server.js
@@ -1,14 +1,36 @@
+const express = require('express');
+const app = express();
 const webpack = require('webpack');
 const WebpackDevServer = require('webpack-dev-server');
 const config = require('./webpack.config');
 
-new WebpackDevServer(webpack(config), {
-  publicPath: config.output.publicPath,
-  hot: true,
-  historyApiFallback: true
-}).listen(5000, 'localhost', (err) => {
-  if (err) {
-    console.log(err);
-  }
-  console.log('Listening at localhost:5000');
-});
+const isDevelopment = process.env.NODE_ENV !== 'production';
+if (isDevelopment) { // DEV SERVER
+  new WebpackDevServer(webpack(config), {
+    publicPath: config.output.publicPath,
+    hot: true,
+    historyApiFallback: true
+  }).listen(5000, 'localhost', (err) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log('Listening at localhost:5000');
+  });
+} else { // PRODUCTION SERVER
+  app.set('port', (process.env.PORT || 5000));
+
+  app.use(express.static(__dirname + '/public'));
+
+  // views is directory for all template files
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+
+  app.get('/', function(request, response) {
+    // response.render('pages/index');
+    response.send('HELLO WORLD');
+  });
+
+  app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+  });
+}
