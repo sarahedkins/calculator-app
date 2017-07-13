b29568687645e08b1b782564a59f023407047a0f
Sarah Edkins <edkins.sarah@gmail.com>
2017-07-13 14:14:29 -0400

    remove dev server

--- server.js
+++ server.js
@@ -1,32 +1,15 @@
 const express = require('express');
 const path = require('path');
-const webpack = require('webpack');
-const WebpackDevServer = require('webpack-dev-server');
-const config = require('./webpack.config');
 
 const app = express();
 
-const isDevelopment = process.env.NODE_ENV !== 'production';
-if (isDevelopment) { // DEV SERVER
-  new WebpackDevServer(webpack(config), {
-    publicPath: config.output.publicPath,
-    hot: true,
-    historyApiFallback: true
-  }).listen(5000, 'localhost', (err) => {
-    if (err) {
-      console.log(err);
-    }
-    console.log('Listening at localhost:5000');
-  });
-} else { // PRODUCTION SERVER
-  app.set('port', (process.env.PORT || 5000));
-  app.use(express.static(path.join(__dirname, '/static')));
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(path.join(__dirname, '/static')));
 
-  app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-  });
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
-  app.listen(app.get('port'), () => {
-    console.log('Node app is running on port', app.get('port'));
-  });
-}
+app.listen(app.get('port'), () => {
+  console.log('Node app is running on port', app.get('port'));
+});
